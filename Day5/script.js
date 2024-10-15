c6b881f99ea8ffe936033361a341efa3fd382c45
chirag-2004 <chiraggupta.ddn@gmail.com>
2024-10-15 15:43:02 +0530

    Arrow function and dom

---
+++ Day5/script.js
@@ -0,0 +1,23 @@
+// let x=confirm("Are you sure to log out");
+// alert(x);
+
+
+// let v=prompt("Enter you name","Chirag");
+// alert(v);
+
+
+
+// let disp = () => {
+//     console.log("Welcome to arrow function in JS");
+// }
+// disp();
+
+// let say=()=>console.log("single arrow function")
+// say();
+
+let sum=(x,y)=>{
+    return(x+y);
+}
+
+let res=sum(12,32);
+console.log(res);
