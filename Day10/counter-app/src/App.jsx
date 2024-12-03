349f5419679bd58e581250b73201603b812e292c
chirag-2004 <chiraggupta.ddn@gmail.com>
2024-12-03 15:01:17 +0530

    Counter-App Using React

--- Day10/counter-app/src/App.jsx
+++ Day10/counter-app/src/App.jsx
@@ -1,28 +1,38 @@
+import "./App.css";
 import { useState } from "react";
-import "./App.css"
 const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div className="count-app">
       <h1 className="count-disp">{count}</h1>
       <div className="count-body">
-        <button className="count-btn"
-          onClick={()=>{
-            setCount(count + 1)
+        <button
+          className="count-btn"
+          onClick={() => {
+            setCount(count + 1);
           }}
-          > Increment
-        </button><br/>
-        <button className="count-btn" onClick={()=>{
-            setCount(0)
+        >
+          Increment
+        </button>
+        <button
+          className="count-btn"
+          onClick={() => {
+            setCount(count - 1);
           }}
-          > Reset</button><br />
-          <button className="count-btn" onClick={()=>{
-              setCount(count-1)
-            }}
-            > Decrement</button>
+        >
+          Decrement
+        </button>
+        <button
+          className="count-btn"
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
-  )
+  );
 };
 
 export default App;
--- Day10/counter-app/src/main.jsx
+++ Day10/counter-app/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ImageEditor } from './ImageEditor'
+import ImageEditor from './ImageEditor'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ImageEditor />
     {/* <App /> */}
-    <ImageEditor/>
+
   </StrictMode>,
-)
+);
