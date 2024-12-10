0c3c3455b0042fa486e147ea5eacedec1c697f65
chirag-2004 <chiraggupta.ddn@gmail.com>
2024-12-10 15:32:56 +0530

    User-Login- App

--- Day12/user-login-app/src/components/Dashboard.jsx
+++ Day12/user-login-app/src/components/Dashboard.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const Dashboard = () => {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const DashBoard = ({ regDash }) => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="jumbotron">
+          <h1>User DashBoard</h1>
+          <h2>Logout</h2>
+          <p>Welcome {regDash.name}</p>
+        </div>
+        <p>You Login Email : {regDash.email}</p>
+        <p>Click the button to Fetch Weather</p>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default DashBoard;
